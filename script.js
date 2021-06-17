f482ade6a00770de5d0e90c0f0d57128bf9b1102
mhdafeef <81076482+mhdafeef@users.noreply.github.com>
2021-06-18 01:32:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,10 @@ $('.butn-front').on('click', function () {
     validate();
 
 /* Disable a button if one of the inputs is empty end */
+
+
+var typed = new Typed(".typing", {
+  strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true});
