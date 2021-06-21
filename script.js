c589e5f4dd7bded6be5f0e966b0cf32cbb112159
mhdafeef <81076482+mhdafeef@users.noreply.github.com>
2021-06-22 01:45:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,3 +50,9 @@ var typed = new Typed(".typing", {
   typeSpeed: 100,
   backSpeed: 60,
   loop: true});
+
+var typed = new Typed(".typing2", {
+  strings: ["Testing Testing", "YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true});
